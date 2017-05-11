7a7f4d28f5e9bed795c8bf2cb53840500080b8a3
NickWoood <nholtz1995@gmail.com>
2017-05-11 17:45:35 +0200

    Create field.h

---
+++ field.h
@@ -0,0 +1,17 @@
+#ifndef field_h
+#define field_h
+
+class field {
+public:
+	field(int size = 10);	//Konstruktor mit Defaultwerten
+	void print();
+	int getEntry(int x, int y);
+	bool isInserted(int thing);
+	void insert(int value, int x, int y);
+
+	int** array;
+private:
+	int length;
+};
+
+#endif
