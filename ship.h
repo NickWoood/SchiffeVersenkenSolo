1156c23938dd9271ed5aa47fe865e8b16db7659c
NickWoood <nholtz1995@gmail.com>
2017-05-14 13:01:38 +0200

    Update ship.h

--- ship.h
+++ ship.h
@@ -2,13 +2,11 @@ class ship {
 public:
 	void initialize();
 	bool isOnPos(int x, int y);
-	void destroy(int x, int y);
+	void attack(int x, int y);
 	void getPos();
 	void addPos(int x, int y);
 
-	int length;
-	int tiles;
+	int length = 0;
+	int tiles = 0;
 	int** pos;
-	static int count;
-
 };
