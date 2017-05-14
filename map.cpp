1de1491da4fe8f149a3e3756b8741aa01bbbe04e
NickWoood <nholtz1995@gmail.com>
2017-05-14 13:00:39 +0200

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -5,45 +5,12 @@
 
 void map::attackField(int x, int y)
 {
-	/*
-	*Alle Schiffe sollen in einer Tabelle gespeichert werden.
-	*Dadurch wird es in dieser Methode nur noch eine Schleife geben.
-	*/
-	bool hit = false;
-	for (int i = 1; i <= s_schiffe_count; i++) {
-		if (s_schiffe[i - 1].isOnPos(x, y)) {
-			s_schiffe[i - 1].destroy(x, y);
-			hit = true;
-			break;
+	if (ship_count > 0) {
+		for (int q = 1; q <= ship_count; q++) {
+			s_objects[q - 1].attack(x, y);
 		}
 	}
-
-	for (int i = 1; i <= k_schiffe_count; i++) {
-		if (k_schiffe[i - 1].isOnPos(x, y)) {
-			k_schiffe[i - 1].destroy(x, y);
-			hit = true;
-			break;
-		}
-	}
-	for (int i = 1; i <= u_schiffe_count; i++) {
-		if (u_schiffe[i - 1].isOnPos(x, y)) {
-			u_schiffe[i - 1].destroy(x, y);
-			hit = true;
-			break;
-		}
-	}
-	for (int i = 1; i <= z_schiffe_count; i++) {
-		if (z_schiffe[i - 1].isOnPos(x, y)) {
-			z_schiffe[i - 1].destroy(x, y);
-			hit = true;
-			break;
-		}
-	}
-
-	if (hit) {
-		std::cout << "Du hast was getroffen !" << std::endl;
-		refresh();
-	}
+	refresh();
 }
 
 
@@ -149,74 +116,37 @@ void map::createShipOnRandomPos(int length,ship* j, int index)
 
 void map::generateShips(int s, int k, int z, int u)
 {
-	if(s > 0)
-		s_schiffe = new ship[s]; 
-	for (s;  s > 0; s--) {
-		createShipOnRandomPos(5, s_schiffe, s-1);
-		s_schiffe_count++;
-
+	std::cout << "Erstelle " << s << " Schlachtschiffe, " << k << " Kreuzer, " << z << " Zerstoerer und " << u << " U-Boote." << std::endl;
+    s_objects = new ship[(s+k+z+u)];
+	ship_count += (s + k + z + u);
+	int j = 0;
+	for (s; s > 0; s--) {
+		createShipOnRandomPos(5, s_objects, j);
+		j++;
 	}
-	if (k > 0)
-		k_schiffe = new ship[k];
 	for (k; k > 0; k--) {
-		createShipOnRandomPos(4, k_schiffe, k-1);
-		k_schiffe_count++;
+		createShipOnRandomPos(4, s_objects, j);
+		j++;
 	}
-	if (z > 0)
-		z_schiffe = new ship[z];
 	for (z; z > 0; z--) {
-		createShipOnRandomPos(3, z_schiffe, z-1);
-		z_schiffe_count++;
+		createShipOnRandomPos(3, s_objects, j);
+		j++;
 	}
-	if (u > 0)
-		u_schiffe = new ship[u];
 	for (u; u > 0; u--) {
-		createShipOnRandomPos(2, u_schiffe,u -1);
-		u_schiffe_count++;
+		createShipOnRandomPos(2, s_objects, j);
+		j++;
 	}
-
-}
-
-int map::getShipCount()
-{
-	return (s_schiffe_count + k_schiffe_count + z_schiffe_count + u_schiffe_count);
 }
 
 void map::refresh()
 {
-	/*
-	*Diese Methode wird noch verkleinert (weniger Schleifen).
-	*Momentan ist hier noch irgendwo ein bug, der zu fehlerhaften Map-Daten führt.
-	*/
-	for(int xpos = 1; xpos <= 10; xpos++) {
-		for (int ypos = 1; ypos <= 10; ypos++) {
-			for (int i = 1; i <= s_schiffe_count; i++) {
-				if (s_schiffe[i - 1].isOnPos(xpos, ypos)) {
-					insert(5, xpos, ypos);
+	clean();
+	for (int x = 1; x <= 10; x++) {
+		for (int y = 1; y <= 10; y++) {
+			for (int q = 1; q <= ship_count; q++) {
+				if (s_objects[q - 1].isOnPos(x, y)) {
+					insert(s_objects[q - 1].length, x, y);
 				}
-				else
-					insert(0, xpos, ypos);
-			}
-			for (int i = 1; i <= k_schiffe_count; i++) {
-				if (k_schiffe[i - 1].isOnPos(xpos, ypos)) {
-					insert(4, xpos, ypos);
-				}
-				else
-					insert(0, xpos, ypos);
-			}
-			for (int i = 1; i <= u_schiffe_count; i++) {
-				if (u_schiffe[i - 1].isOnPos(xpos, ypos)) {
-					insert(2, xpos, ypos);
-				}
-				else
-					insert(0, xpos, ypos);
-			}
-			for (int i = 1; i <= z_schiffe_count; i++) {
-				if (z_schiffe[i - 1].isOnPos(xpos, ypos)) {
-					insert(3, xpos, ypos);
-				}
-				else
-					insert(0, xpos, ypos);
 			}
 		}
 	}
